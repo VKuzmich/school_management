fcf8c708e0d2235f0ae49f34d7c5c99ef12d9cfe
Vladimir Kuzmich <vjk1976@ukr.net>
2020-01-20 12:53:32 +0200

    pagination and breadcrumbs

--- app/controllers/admin/teachers_controller.rb
+++ app/controllers/admin/teachers_controller.rb
@@ -1,11 +1,13 @@
 class Admin::TeachersController < Admin::BaseController
+  add_breadcrumb 'Teachers', :admin_teachers_path
   before_action :set_teacher, only: [:edit, :update, :destroy]
 
   def index
-    @teachers = Teacher.order(id: :desc)
+    @teachers = Teacher.order(id: :desc).page(params[:page])
   end
 
   def new
+    add_breadcrumb 'New Teacher', new_admin_teacher_path
     @teacher = Teacher.new
   end
 
@@ -15,18 +17,22 @@ class Admin::TeachersController < Admin::BaseController
     if @teacher.save
       redirect_to admin_teachers_path, notice: "Teacher was added"
     else
+      add_breadcrumb 'New Teacher', new_admin_teacher_path
       flash.now[:alert] = 'Could not create teacher'
       render :new
     end
   end
 
   def edit
+    add_breadcrumb "Edit Teacher #{@teacher.first_name} #{@teacher.last_name}", [:edit, :admin, @teacher]
   end
 
   def update
     if @teacher.update(teacher_params)
       redirect_to admin_teachers_path, notice: "Teacher was edited"
     else
+      add_breadcrumb "Edit Teacher #{@teacher.first_name} #{@teacher.last_name}", [:admin, @teacher]
+
       flash.now[:alert] = 'Could not edit teacher'
       render :edit
     end
